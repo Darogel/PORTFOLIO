f209f3b3f5898456e858b4e591eb8bbfcc049f1c
darogel <41120975+darogel@users.noreply.github.com>
2021-05-26 22:48:19 -0500

    Personalización del Topbar Inicial

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,23 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo"></a>
+          <div className="itemContainer">
+            <Person />
+            <span>+593 988676813</span>
+          </div>
+          <div className="itemContainer">
+            <Mail />
+            <span>darogel96@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
